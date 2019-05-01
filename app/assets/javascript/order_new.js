b91e6641379119c93250c373e6c47ffce1944464
Rafael Moraes <roliveira.moraes@gmail.com>
2019-05-01 17:33:57 -0300

    Implements js to autofill the user attributes when cpf registered

---
+++ app/assets/javascript/order_new.js
@@ -0,0 +1,42 @@
+(() => {
+  const User = () => {
+    const cpfRegex = /^[0-9]{3}\.[0-9]{3}\.[0-9]{3}-[0-9]{2}$/
+
+    const id = document.querySelector('#order_user_id');
+    const name = document.querySelector('#order_user_name');
+    const email = document.querySelector('#order_user_email');
+    const cpf = document.querySelector('#order_user_cpf');
+
+    const isCPFValid = (rawCPF) => cpfRegex.test(rawCPF.trim())
+
+    const clear = () => {
+      name.value = '';
+      email.value = '';
+    }
+
+    const fill = (user) => {
+      id.value = user.id;
+      name.value = user.name;
+      cpf.value = user.cpf;
+      email.value = user.email;
+    }
+
+    const findByCPF = (rawCPF) => {
+      if (isCPFValid(rawCPF)) {
+        fetch(`http://localhost:3000/users/search?cpf=${cpf.value}`)
+          .then(response => response.json())
+          .then(response => fill(response.user))
+          .catch(clear)
+      } else {
+        clear();
+      }
+    }
+
+    return { findByCPF: findByCPF };
+  }
+
+  document.querySelector("#order_user_cpf")
+          .addEventListener('change', function() {
+    User().findByCPF(this.value);
+  });
+})();
