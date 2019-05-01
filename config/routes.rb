5fb4bf072c62c6945b1773b19dd77fe442be2ec5
Rafael Moraes <roliveira.moraes@gmail.com>
2019-05-01 17:29:07 -0300

    Configures Orders routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  defaults format: :html do
+    resources :orders, only: %i[index new create]
+    root to: 'orders#new'
+  end
 end
