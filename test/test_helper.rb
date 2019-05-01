438cc30c2cd7035e92f9ad210df3dee7c4a56df1
Rafael Moraes <roliveira.moraes@gmail.com>
2019-05-01 10:18:11 -0300

    Configures webmock and simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,14 @@
+SimpleCov.start
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+require 'webmock/minitest'
+
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
+  # parallelize(workers: :number_of_processors)
 
   # Add more helper methods to be used by all tests here...
 end
